ee62f25f705206559c5dec6f77d2ae5626bb2ef2
mithridatem <mathieumithridate@adrar-formation.com>
2023-03-21 16:16:48 +0100

    mise en place du router

---
+++ App/Vue/viewAddUser.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter un Utilisateur</title>
+</head>
+<body>
+    <h1>Ajouter un Utilisateur</h1>
+</body>
+</html>
