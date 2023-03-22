451cb4691e2b1e43bbaa8da404c202da62e1ccfb
mithridatem <mathieumithridate@adrar-formation.com>
2023-03-22 12:04:59 +0100

    ajout de la classe Fonctions

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -1,9 +1,23 @@
 <?php
     namespace App\Controller;
     use App\Model\Utilisateur;
+    use App\Utils\Fonctions;
     class UserController extends Utilisateur{
         //fonction qui va gérer l'ajout d'un utilisateur en BDD
         public function insertUser(){
+            /*-------------------------------
+                        logique 
+            --------------------------------*/
+            //test si le bouton est cliqué 
+            if(isset($_POST['submit'])){
+                //récupération et nettoyage des inputs utilisateurs
+                $nom = Fonctions::cleanInput($_POST['nom_utilisateur']);
+                $prenom = Fonctions::cleanInput($_POST['prenom_utilisateur']);
+                $mail = Fonctions::cleanInput($_POST['mail_utilisateur']);
+                $password = Fonctions::cleanInput($_POST['password_utilisateur']);
+                //tester si les champs sont remplis
+                if()
+            }
             //importer la vue
             include './App/Vue/viewAddUser.php';
         }
