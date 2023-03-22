e41f496807d6b8e7567ea6a89a7842019d3f1590
mithridatem <mathieumithridate@adrar-formation.com>
2023-03-22 12:24:11 +0100

    ajout de la logique -> fonction insertUser

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -5,6 +5,8 @@
     class UserController extends Utilisateur{
         //fonction qui va gérer l'ajout d'un utilisateur en BDD
         public function insertUser(){
+            //variable pour stocker les messages d'erreurs
+            $msg = "";
             /*-------------------------------
                         logique 
             --------------------------------*/
@@ -16,7 +18,25 @@
                 $mail = Fonctions::cleanInput($_POST['mail_utilisateur']);
                 $password = Fonctions::cleanInput($_POST['password_utilisateur']);
                 //tester si les champs sont remplis
-                if()
+                if(!empty($nom) AND !empty($prenom)AND !empty($mail) AND !empty($password)){
+                    //hash du mot de passe
+                    $password = password_hash($password, PASSWORD_DEFAULT);
+                    $user = new Utilisateur();
+                    $user->setNomUtilisateur($nom);
+                    $user->setPrenomUtilisateur($prenom);
+                    $user->setMailUtilisateur($mail);
+                    $user->setPasswordUtilisateur($password);
+                    //version alternative avec $this
+                    /* $this->setNomUtilisateur($nom);
+                    $this->setPrenomUtilisateur($prenom);
+                    $this->setMailUtilisateur($mail);
+                    $this->setpasswordUtilisateur($password); */
+                    var_dump($user);
+                }
+                //sinon si les champs ne sont pas tous remplis
+                else{
+                    $msg = "Veuillez remplir tous les champs du formulaire";
+                }
             }
             //importer la vue
             include './App/Vue/viewAddUser.php';
