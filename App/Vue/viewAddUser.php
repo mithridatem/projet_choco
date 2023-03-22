f8b0d43b9431585760849a39f1fcacfbdff3b9ad
mithridatem <mathieumithridate@adrar-formation.com>
2023-03-22 10:22:33 +0100

    update model et vue

--- App/Vue/viewAddUser.php
+++ App/Vue/viewAddUser.php
@@ -1,4 +1,4 @@
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -6,6 +6,17 @@
     <title>Ajouter un Utilisateur</title>
 </head>
 <body>
-    <h1>Ajouter un Utilisateur</h1>
+    <h3>Ajouter un compte utilisateur :</h3>
+    <form action="" method="post">
+        <label for="nom_utilisateur">saisir votre nom :</label>
+        <input type="text" name="nom_utilisateur">
+        <label for="prenom_utilisateur">saisir votre prénom :</label>
+        <input type="text" name="prenom_utilisateur">
+        <label for="mail_utilisateur">saisir votre mail :</label>
+        <input type="email" name="mail_utilisateur">
+        <label for="password_utilisateur">saisir votre mot de passe :</label>
+        <input type="password" name="password_utilisateur">
+        <input type="submit" value="Ajouter" name="submit">
+    </form>
 </body>
 </html>
