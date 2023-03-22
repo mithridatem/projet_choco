8abe7f673cf27f8c1f7f08422dcbeb40363b9944
mithridatem <mathieumithridate@adrar-formation.com>
2023-03-22 11:30:39 +0100

    ajout de la fonction insertuser

--- App/Model/Utilisateur.php
+++ App/Model/Utilisateur.php
@@ -19,7 +19,7 @@
         ------------------------*/
         public function __construct(){
             //instancier un objet role quand on créé un
-            $this->roles = new Roles('user');
+            //$this->roles = new Roles('user');
         }
         /*-----------------------
             Getters et Setters
@@ -51,7 +51,33 @@
         public function setPasswordUtilisateur($pwd):void{
             $this->password_utilisateur = $pwd;
         }
-        
+        /*-----------------------
+                Méthodes
+        ------------------------*/
+        //méthode pour ajouter un utilisateur en BDD
+        public function addUser():void{
+            try {
+                //récupérer les données
+                $nom = $this->nom_utilisateur;
+                $prenom = $this->prenom_utilisateur;
+                $mail = $this->mail_utilisateur;
+                $password = $this->password_utilisateur;
+                //préparer la requête
+                $req = $this->connexion()->prepare('INSERT INTO utilisateur(nom_utilisateur, 
+                prenom_utilisateur, mail_utilisateur, password_utilisateur) VALUES(?,?,?,?)');
+                //bind les paramètres
+                $req->bindParam(1, $nom, \PDO::PARAM_STR);
+                $req->bindParam(2, $prenom, \PDO::PARAM_STR);
+                $req->bindParam(3, $mail, \PDO::PARAM_STR);
+                $req->bindParam(4, $password, \PDO::PARAM_STR);
+                //Exécuter la requête
+                $req->execute();
+            } 
+            catch (\Exception $e) {
+                die('Erreur : '.$e->getMessage());
+            }
+            
+        }
     }
 
 ?>
