9d580f83eba121ca7957c412b37bdc6972ec58cf
mithridatem <mathieumithridate@adrar-formation.com>
2023-03-23 11:49:36 +0100

    ajout du style css

--- App/Vue/viewAddUser.php
+++ App/Vue/viewAddUser.php
@@ -3,21 +3,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Public/Style/main.css">
+    <script src="./Public/Script/script.js" defer></script>
     <title>Ajouter un Utilisateur</title>
 </head>
 <body>
-    <h3>Ajouter un compte utilisateur :</h3>
-    <form action="" method="post">
-        <label for="nom_utilisateur">saisir votre nom :</label>
-        <input type="text" name="nom_utilisateur">
-        <label for="prenom_utilisateur">saisir votre prénom :</label>
-        <input type="text" name="prenom_utilisateur">
-        <label for="mail_utilisateur">saisir votre mail :</label>
-        <input type="email" name="mail_utilisateur">
-        <label for="password_utilisateur">saisir votre mot de passe :</label>
-        <input type="password" name="password_utilisateur">
-        <input type="submit" value="Ajouter" name="submit">
-    </form>
-    <div id="error"><?php echo $msg; ?></div>
+    <div class="form">
+        <h3>Ajouter un compte utilisateur :</h3>
+        <form action="" method="post">
+            <label for="nom_utilisateur">saisir votre nom :</label>
+            <input type="text" name="nom_utilisateur">
+            <label for="prenom_utilisateur">saisir votre prénom :</label>
+            <input type="text" name="prenom_utilisateur">
+            <label for="mail_utilisateur">saisir votre mail :</label>
+            <input type="email" name="mail_utilisateur">
+            <label for="password_utilisateur">saisir votre mot de passe :</label>
+            <input type="password" name="password_utilisateur">
+            <input type="submit" value="Ajouter" name="submit">
+        </form>
+        <div id="error"><?php echo $msg; ?></div>
+    </div>
 </body>
 </html>
---
+++ App/Vue/viewMenu.php
@@ -0,0 +1,6 @@
+<div id="navbar">
+    <li><a href="./">Home</a></li>
+    <li><a href="./userAdd">Inscription</a></li>
+    <li><a href="./rolesAdd">Ajouter Role</a></li>
+    <li><a href="./">Connexion</a></li>
+</div>
