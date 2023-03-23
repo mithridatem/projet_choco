9c2b712978ac0d592cf0eb8f6a7ca6585bf34458
mithridatem <mathieumithridate@adrar-formation.com>
2023-03-23 12:00:34 +0100

    ajout du menu + style css

--- App/Vue/home.php
+++ App/Vue/home.php
@@ -3,9 +3,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Public/Style/main.css">
+    <script src="./Public/Script/script.js" defer></script>
     <title>Home</title>
 </head>
 <body>
+    <!--import du menu -->
+    <?php include './App/Vue/viewMenu.php';?>
     <h1>Bienvenue sur Chocoblast</h1>
 </body>
 </html>
