ca5e8dcbe7c538c84af3775c8caea83a36d8e868
mithridatem <mathieumithridate@adrar-formation.com>
2023-03-24 16:32:53 +0100

    Ajout de la fonction showAllChocoblast

--- App/Vue/viewMenu.php
+++ App/Vue/viewMenu.php
@@ -6,6 +6,8 @@
     <div id="navbar">
         <li><a href="./">Home</a></li>
         <li><a href="./chocoblastAdd">Ajouter chocoblast</a></li>
+        <li><a href="./chocoblastAll">Afficher chocoblast</a></li>
+        <li><a href="./rolesAdd">Ajouter Role</a></li>
         <li><a href="./deconnexion">Déconnexion</a></li>
     </div>
 <?php
@@ -17,7 +19,7 @@
     <div id="navbar">
         <li><a href="./">Home</a></li>
         <li><a href="./userAdd">Inscription</a></li>
-        <li><a href="./rolesAdd">Ajouter Role</a></li>
+        <li><a href="./chocoblastAll">Afficher chocoblast</a></li>
         <li><a href="./connexion">Connexion</a></li>
     </div>
 <?php
