30a46f08c9843508ca4982d29b218a28bd05b2af
mithridatem <mathieumithridate@adrar-formation.com>
2023-06-20 00:23:01 +0200

    env

---
+++ env.php
@@ -0,0 +1,11 @@
+<?php
+    $host= "localhost";
+    $database = "chocoblast";
+    $login= "root";
+    $password = "";
+    $secretKey  = 'wV4Dapfxi8ppnvgSAQdA8WgCd7ULyRORu5GuG7kb6w2PKtFbkvfY2SVBagOm
+        +n8wfRIA/jgwKi9vOtzNfaf+QgG/MKDJ/EvjwWq3VSFNswfXI8FvPYLmFh7r
+        pPs38j6n0kcBZVw5n5tihtMEJdO+4AojsovTfAbJkyV0O+JjL5JhfkAYdi2e
+        QgbD6RTJJT1mDP2FcsdWUqvLEl0vaeBKo51n/Y9QP1gbKw==
+        =4KAY';
+?>
