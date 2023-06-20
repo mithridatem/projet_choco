66e0fa92118e4d11506b3234d7712776be2f13b3
mithridatem <72612782+mithridatem@users.noreply.github.com>
2023-06-20 04:08:10 +0200

    foreach factorisation

--- Public/Script/script2.js
+++ Public/Script/script2.js
@@ -9,7 +9,16 @@ check.forEach(e=>{
     e.addEventListener('change',()=>{
         if(e.checked){
             let id =e.value;
-            if(id==1){
+            console.log('id coché : '+id);
+            check.forEach(el=>{
+                //si la checkbox est différente de la checkbox sélectionnée
+                if(el.value!=id){
+                    //on passe la checkbox à false (déchoché)
+                    el.checked = false;
+                    console.log('checkbox '+el.value+' déchoché');
+                }
+            });
+            /* if(id==1){
                 check[1].checked = false;
                 check[2].checked = false;
                 check[3].checked = false;
@@ -32,7 +41,7 @@ check.forEach(e=>{
                 check[1].checked = false;
                 check[2].checked = false;
                 id = 4;
-            }
+            } */
             fetch("http://localhost:8080/projet_choco/api/chocoFilter?filter="+id)
             .then(async response =>{
                 console.log(await response.json());
